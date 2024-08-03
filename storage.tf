0618b0b52d03153c422f55e3a01143e543e2b989
viniciusvec <vinicius_vec@hotmail.com>
2024-08-03 17:04:01 +0100

    re-enabled RDS deployment

--- storage.tf
+++ storage.tf
@@ -29,47 +29,47 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 }
 
 
-# # RDS Cluster / Aurora
-# #https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/aurora-serverless-v2.how-it-works.html
-# resource "aws_rds_cluster" "rds_cluster" {
-#   cluster_identifier = "aurora-cluster"
-#   engine             = "aurora-mysql"
-#   engine_mode        = "provisioned"
-#   engine_version     = "8.0"
-#   database_name      = "rds_db"
-#   master_username    = var.db_username
-#   #master_password    = not in use due to below
-#   manage_master_user_password = true
+# RDS Cluster / Aurora
+#https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/aurora-serverless-v2.how-it-works.html
+resource "aws_rds_cluster" "rds_cluster" {
+  cluster_identifier = "aurora-cluster"
+  engine             = "aurora-mysql"
+  engine_mode        = "provisioned"
+  engine_version     = "8.0"
+  database_name      = "rds_db"
+  master_username    = var.db_username
+  #master_password    = not in use due to below
+  manage_master_user_password = true
 
-#   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-#   availability_zones   = var.availability_zones
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  availability_zones   = var.availability_zones
 
-#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
-#   storage_encrypted   = true
-#   apply_immediately   = true
-#   skip_final_snapshot = true # consider removing for production
+  storage_encrypted   = true
+  apply_immediately   = true
+  skip_final_snapshot = true # consider removing for production
 
-#   serverlessv2_scaling_configuration {
-#     max_capacity = 2.0
-#     min_capacity = 1.0
-#   }
+  serverlessv2_scaling_configuration {
+    max_capacity = 2.0
+    min_capacity = 1.0
+  }
 
-#   tags = {
-#     Name = "aurora-cluster"
-#   }
-# }
+  tags = {
+    Name = "aurora-cluster"
+  }
+}
 
-# resource "aws_rds_cluster_instance" "aurora_instance" {
-#   identifier         = "aurora-cluster-instance-${count.index}"
-#   count              = 1 # Size reduced to speed up dev environment
-#   cluster_identifier = aws_rds_cluster.rds_cluster.id
-#   instance_class     = "db.serverless"
-#   engine             = aws_rds_cluster.rds_cluster.engine
-#   engine_version     = aws_rds_cluster.rds_cluster.engine_version
+resource "aws_rds_cluster_instance" "aurora_instance" {
+  identifier         = "aurora-cluster-instance-${count.index}"
+  count              = 1 # Size reduced to speed up dev environment
+  cluster_identifier = aws_rds_cluster.rds_cluster.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.rds_cluster.engine
+  engine_version     = aws_rds_cluster.rds_cluster.engine_version
 
-#   timeouts {
-#     create = "30m"
-#     delete = "60m"
-#   }
-# }
+  timeouts {
+    create = "30m"
+    delete = "60m"
+  }
+}
