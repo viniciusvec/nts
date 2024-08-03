9948717e4ff8e492434db1f6c7f4e954f803b9ac
viniciusvec <vinicius_vec@hotmail.com>
2024-08-03 08:56:29 +0100

    added sn 3 rt

--- main.tf
+++ main.tf
@@ -87,6 +87,11 @@ resource "aws_route_table_association" "public_rt_assoc2" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_route_table_association" "public_rt_assoc3" {
+  subnet_id      = aws_subnet.public_subnet3.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 # Private Route Table 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
@@ -105,5 +110,10 @@ resource "aws_route_table_association" "private_rt_assoc1" {
 
 resource "aws_route_table_association" "private_rt_assoc2" {
   subnet_id      = aws_subnet.private_subnet2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_assoc3" {
+  subnet_id      = aws_subnet.private_subnet3.id
   route_table_id = aws_route_table.private_rt.id
 }
