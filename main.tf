d275a310fe287f257701f984f646f34277a283e3
viniciusvec <vinicius_vec@hotmail.com>
2024-08-03 10:00:37 +0100

    fixed sn overlap and removed nat gateway

--- main.tf
+++ main.tf
@@ -27,20 +27,20 @@ resource "aws_subnet" "public_subnet3" {
 
 resource "aws_subnet" "private_subnet1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = var.availability_zones[0]
 
 }
 
 resource "aws_subnet" "private_subnet2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
+  cidr_block        = "10.0.5.0/24"
   availability_zone = var.availability_zones[1]
 }
 
 resource "aws_subnet" "private_subnet3" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.5.0/24"
+  cidr_block        = "10.0.6.0/24"
   availability_zone = var.availability_zones[2]
 }
 
@@ -51,17 +51,17 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# Elastic IP for NAT Gateway 
-resource "aws_eip" "nat_eip" {
-  domain     = "vpc"
-  depends_on = [aws_internet_gateway.igw]
-}
+# # Elastic IP for NAT Gateway 
+# resource "aws_eip" "nat_eip" {
+#   domain     = "vpc"
+#   depends_on = [aws_internet_gateway.igw]
+# }
 
-# NAT Gateway 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet1.id
-}
+# # NAT Gateway 
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.public_subnet1.id
+# }
 
 ################################### Route tables
 
@@ -93,14 +93,14 @@ resource "aws_route_table_association" "public_rt_assoc3" {
 }
 
 # Private Route Table 
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_route_table" "private_rt" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-}
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat.id
+#   }
+# }
 
 # Private Route Table Association 
 resource "aws_route_table_association" "private_rt_assoc1" {
